f16dd7dd7096235f5e772631722f476252eb4e68
Romain Beaumont <romain.rom1@gmail.com>
2021-08-11 23:27:02 +0000

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         name = 'clip_retrieval',
         packages = find_packages(),
         include_package_data = True,
-        version = '1.0.1',
+        version = '1.0.0',
         license='MIT',
         description = 'Easily computing clip embeddings and building a clip retrieval system with them',
         long_description=long_description,
