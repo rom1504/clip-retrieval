73ab380fb9c00121b546d8a338fba1cab875a748
Romain Beaumont <romain.rom1@gmail.com>
2021-09-04 14:23:26 +0200

    Update gh-pages

---
+++ vendor/custom-elements-es5-adapter.js
@@ -0,0 +1,2 @@
+/*! For license information please see custom-elements-es5-adapter.js.LICENSE.txt */
+!function(){"use strict";!function(){if(void 0===window.Reflect||void 0===window.customElements||window.customElements.polyfillWrapFlushCallback)return;const t=HTMLElement;window.HTMLElement={HTMLElement:function(){return Reflect.construct(t,[],this.constructor)}}.HTMLElement,HTMLElement.prototype=t.prototype,HTMLElement.prototype.constructor=HTMLElement,Object.setPrototypeOf(HTMLElement,t)}()}();
