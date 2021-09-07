6b6717156f2ed12ec79f9ba756f3030643b43103
Romain Beaumont <romain.rom1@gmail.com>
2021-09-08 00:51:44 +0200

    expose max_index_memory_usage in clip index

--- README.md
+++ README.md
@@ -75,6 +75,8 @@ Clip index takes as input the output of clip inference and makes an index out of
 
 `clip-retrieval index --input_folder embeddings_folder --output_folder index_folder`
 
+An additional `--max_index_memory_usage "4G"` option allow configuring the amount of ram the index will consume. More ram, better knn recall.
+
 The output is a folder containing:
 * image.index containing a brute force faiss index for images
 * text.index containing a brute force faiss index for texts
--- clip_retrieval/clip_index.py
+++ clip_retrieval/clip_index.py
@@ -5,15 +5,15 @@ from glob import glob
 import os
 from distutils.dir_util import copy_tree
 
-def clip_index(embeddings_folder, index_folder):
+def clip_index(embeddings_folder, index_folder, max_index_memory_usage="4G"):
     quantizer = Quantizer()
     if os.path.exists(embeddings_folder+"/img_emb"):
-        quantizer.quantize(embeddings_path=embeddings_folder+"/img_emb", output_path=index_folder+"/img_index", max_index_memory_usage="16G", current_memory_available="16G")
+        quantizer.quantize(embeddings_path=embeddings_folder+"/img_emb", output_path=index_folder+"/img_index", max_index_memory_usage=max_index_memory_usage, current_memory_available="16G")
         index_file = list(glob(index_folder+"/img_index/*.index"))[0]
         shutil.move(index_file, index_folder+"/image.index")
         os.rmdir(index_folder+"/img_index")
     if os.path.exists(embeddings_folder+"/text_emb"):
-        quantizer.quantize(embeddings_path=embeddings_folder+"/text_emb", output_path=index_folder+"/txt_index", max_index_memory_usage="16G", current_memory_available="16G")
+        quantizer.quantize(embeddings_path=embeddings_folder+"/text_emb", output_path=index_folder+"/txt_index", max_index_memory_usage=max_index_memory_usage, current_memory_available="16G")
         index_file = list(glob(index_folder+"/txt_index/*.index"))[0]
         shutil.move(index_file, index_folder+"/text.index")
         os.rmdir(index_folder+"/txt_index")
