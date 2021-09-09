90667cadee884ada6cd4738e3d2aa78a9e7aad66
Romain Beaumont <romain.rom1@gmail.com>
2021-09-10 01:37:28 +0200

    use subprocesses in clip index to avoid a memleak at the end of autofaiss

--- clip_retrieval/clip_index.py
+++ clip_retrieval/clip_index.py
@@ -4,21 +4,27 @@ import shutil
 from glob import glob
 import os
 from distutils.dir_util import copy_tree
-import gc
+from multiprocessing import Process
+
+
+def quantize(emb_folder, index_folder, subfolder_name, index_name, max_index_memory_usage, current_memory_available):
+    tmp_output_folder = index_folder+"/"+subfolder_name
+    if os.path.exists(emb_folder):
+        quantizer = Quantizer()
+        quantizer.quantize(embeddings_path=emb_folder, output_path=tmp_output_folder, max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available)
+        index_file = list(glob(tmp_output_folder+"/*.index"))[0]
+        shutil.move(index_file, index_folder+"/"+index_name)
+        os.rmdir(tmp_output_folder)
 
 def clip_index(embeddings_folder, index_folder, max_index_memory_usage="4G", current_memory_available="16G", copy_metadata=True):
-    quantizer = Quantizer()
-    if os.path.exists(embeddings_folder+"/img_emb"):
-        quantizer.quantize(embeddings_path=embeddings_folder+"/img_emb", output_path=index_folder+"/img_index", max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available)
-        index_file = list(glob(index_folder+"/img_index/*.index"))[0]
-        shutil.move(index_file, index_folder+"/image.index")
-        os.rmdir(index_folder+"/img_index")
-    gc.collect()
-    if os.path.exists(embeddings_folder+"/text_emb"):
-        quantizer.quantize(embeddings_path=embeddings_folder+"/text_emb", output_path=index_folder+"/txt_index", max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available)
-        index_file = list(glob(index_folder+"/txt_index/*.index"))[0]
-        shutil.move(index_file, index_folder+"/text.index")
-        os.rmdir(index_folder+"/txt_index")
+    p = Process(target=quantize, args=(embeddings_folder+"/img_emb", index_folder, "img_index", 
+        "image.index", max_index_memory_usage, current_memory_available))
+    p.start()
+    p.join()
+    p = Process(target=quantize, args=(embeddings_folder+"/text_emb", index_folder, "txt_index",
+    "text.index", max_index_memory_usage, current_memory_available))
+    p.start()
+    p.join()
     if copy_metadata:
         copy_tree(embeddings_folder+"/metadata", index_folder+"/metadata")
 
