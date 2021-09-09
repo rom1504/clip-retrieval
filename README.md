fe6bb06cb99bbb5e352849fe5cc066d861d4dfc1
Romain Beaumont <romain.rom1@gmail.com>
2021-09-09 17:07:03 +0200

    add copy metadata option to clip index

--- README.md
+++ README.md
@@ -77,6 +77,7 @@ Clip index takes as input the output of clip inference and makes an index out of
 
 An additional `--max_index_memory_usage "4G"` option allow configuring the amount of ram the index will consume. More ram, better knn recall.
 `--current_memory_available 16G` allows controlling how much ram is used during the creation process.
+`--copy_metadata True` makes it possible to choose whether to copy metadata or not at the end of the process.
 
 The output is a folder containing:
 * image.index containing a brute force faiss index for images
--- clip_retrieval/clip_index.py
+++ clip_retrieval/clip_index.py
@@ -4,20 +4,23 @@ import shutil
 from glob import glob
 import os
 from distutils.dir_util import copy_tree
+import gc
 
-def clip_index(embeddings_folder, index_folder, max_index_memory_usage="4G", current_memory_available="16G"):
+def clip_index(embeddings_folder, index_folder, max_index_memory_usage="4G", current_memory_available="16G", copy_metadata=True):
     quantizer = Quantizer()
     if os.path.exists(embeddings_folder+"/img_emb"):
         quantizer.quantize(embeddings_path=embeddings_folder+"/img_emb", output_path=index_folder+"/img_index", max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available)
         index_file = list(glob(index_folder+"/img_index/*.index"))[0]
         shutil.move(index_file, index_folder+"/image.index")
         os.rmdir(index_folder+"/img_index")
+    gc.collect()
     if os.path.exists(embeddings_folder+"/text_emb"):
         quantizer.quantize(embeddings_path=embeddings_folder+"/text_emb", output_path=index_folder+"/txt_index", max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available)
         index_file = list(glob(index_folder+"/txt_index/*.index"))[0]
         shutil.move(index_file, index_folder+"/text.index")
         os.rmdir(index_folder+"/txt_index")
-    copy_tree(embeddings_folder+"/metadata", index_folder+"/metadata")
+    if copy_metadata:
+        copy_tree(embeddings_folder+"/metadata", index_folder+"/metadata")
 
 
 if __name__ == '__main__':
