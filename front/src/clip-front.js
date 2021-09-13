9c216fd50d39ba76635e739417ae6f05498c5acd
Richard Vencu <vencu.richard@gmail.com>
2021-09-13 16:20:43 +0300

    Revert "caption fix"
    This reverts commit c39a4718edca11b8b5c28591d64e4f6c7b92ad8d.

--- front/src/clip-front.js
+++ front/src/clip-front.js
@@ -147,8 +147,6 @@ class ClipFront extends LitElement {
       caption-side: bottom;
       background: #fff;
       padding: 0 0px 0px;
-      max-width: 170px;
-      word-wrap: break-word;
     }
 
     #searchBar, #searchBar:hover, #searchBar:focus, #searchBar:valid {
