512d4a872145fe5ac9446a876c731ee5050cd2c3
Romain Beaumont <romain.rom1@gmail.com>
2021-11-19 21:48:06 +0100

    update to version 2.0.0 of autofaiss

--- clip_retrieval/clip_index.py
+++ clip_retrieval/clip_index.py
@@ -1,37 +1,30 @@
-from autofaiss.external.quantize import Quantizer
+from autofaiss import build_index
 import fire
-import shutil
-from glob import glob
 import os
 from distutils.dir_util import copy_tree
 from multiprocessing import Process
-import traceback
-import logging
 
-def quantize(emb_folder, index_folder, subfolder_name, index_name, max_index_memory_usage, current_memory_available, nb_cores):
+def quantize(emb_folder, index_folder, index_name, max_index_memory_usage, current_memory_available, nb_cores):
     try:
-        tmp_output_folder = index_folder+"/"+subfolder_name
         if os.path.exists(emb_folder):
-            quantizer = Quantizer()
-            quantizer.quantize(embeddings_path=emb_folder, output_path=tmp_output_folder, 
-            max_index_memory_usage=max_index_memory_usage, current_memory_available=current_memory_available, nb_cores=nb_cores)
-            indices = list(glob(tmp_output_folder+"/*.index"))
-            if len(indices) == 0:
-                raise Exception("No index generated")
-            index_file = indices[0]
-            shutil.move(index_file, index_folder+"/"+index_name)
-            os.rmdir(tmp_output_folder)
+            build_index(
+                embeddings_path=emb_folder,
+                index_path=index_folder+"/"+index_name+".index",
+                index_infos_path=index_folder+"/"+index_name+".json", 
+                max_index_memory_usage=max_index_memory_usage,
+                current_memory_available=current_memory_available,
+                nb_cores=nb_cores)
     except Exception as e:
         print(e)
 
 def clip_index(embeddings_folder, index_folder, max_index_memory_usage="4G", current_memory_available="16G", 
             copy_metadata=True, image_subfolder="img_emb", text_subfolder="text_emb", nb_cores=None):
-    p = Process(target=quantize, args=(embeddings_folder+"/"+image_subfolder, index_folder, "img_index", 
-        "image.index", max_index_memory_usage, current_memory_available, nb_cores))
+    p = Process(target=quantize, args=(embeddings_folder+"/"+image_subfolder, index_folder, 
+        "image", max_index_memory_usage, current_memory_available, nb_cores))
     p.start()
     p.join()
-    p = Process(target=quantize, args=(embeddings_folder+"/"+text_subfolder, index_folder, "txt_index",
-    "text.index", max_index_memory_usage, current_memory_available, nb_cores))
+    p = Process(target=quantize, args=(embeddings_folder+"/"+text_subfolder, index_folder,
+    "text", max_index_memory_usage, current_memory_available, nb_cores))
     p.start()
     p.join()
     if copy_metadata:
