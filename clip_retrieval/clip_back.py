43bd3f9374fec5cee589a6f8ac9b2ef1e1b68e9b
Romain Beaumont <romain.rom1@gmail.com>
2022-03-26 13:16:00 +0100

    support ViT-B/32 safety model
    thanks @christophschuhmann for training it

--- clip_retrieval/clip_back.py
+++ clip_retrieval/clip_back.py
@@ -580,7 +580,7 @@ def load_metadata_provider(
     return metadata_provider, ivf_old_to_new_mapping
 
 
-def load_safety_model():
+def load_safety_model(clip_model):
     """load the safety model"""
     import autokeras as ak  # pylint: disable=import-outside-toplevel
     from tensorflow.keras.models import load_model  # pylint: disable=import-outside-toplevel
@@ -588,17 +588,29 @@ def load_safety_model():
 
     home = expanduser("~")
 
-    cache_folder = home + "/.cache/clip_retrieval"
-    model_dir = cache_folder + "/clip_autokeras_binary_nsfw"
+    cache_folder = home + "/.cache/clip_retrieval/" + clip_model.replace("/", "_")
+    if clip_model == "ViT-L/14":
+        model_dir = cache_folder + "/clip_autokeras_binary_nsfw"
+        dim = 768
+    elif clip_model == "ViT-B/32":
+        model_dir = cache_folder + "/clip_autokeras_nsfw_b32"
+        dim = 512
+    else:
+        raise ValueError("Unknown clip model")
     if not os.path.exists(model_dir):
         os.makedirs(cache_folder, exist_ok=True)
 
         from urllib.request import urlretrieve  # pylint: disable=import-outside-toplevel
 
         path_to_zip_file = cache_folder + "/clip_autokeras_binary_nsfw.zip"
-        url_model = (
-            "https://raw.githubusercontent.com/LAION-AI/CLIP-based-NSFW-Detector/main/clip_autokeras_binary_nsfw.zip"
-        )
+        if clip_model == "ViT-L/14":
+            url_model = "https://raw.githubusercontent.com/LAION-AI/CLIP-based-NSFW-Detector/main/clip_autokeras_binary_nsfw.zip"
+        elif clip_model == "ViT-B/32":
+            url_model = (
+                "https://raw.githubusercontent.com/LAION-AI/CLIP-based-NSFW-Detector/main/clip_autokeras_nsfw_b32.zip"
+            )
+        else:
+            raise ValueError("Unknown model {}".format(clip_model))
         urlretrieve(url_model, path_to_zip_file)
         import zipfile  # pylint: disable=import-outside-toplevel
 
@@ -606,7 +618,7 @@ def load_safety_model():
             zip_ref.extractall(cache_folder)
 
     loaded_model = load_model(model_dir, custom_objects=ak.CUSTOM_OBJECTS)
-    loaded_model.predict(np.random.rand(10 ** 3, 768).astype("float32"), batch_size=10 ** 3)
+    loaded_model.predict(np.random.rand(10 ** 3, dim).astype("float32"), batch_size=10 ** 3)
 
     return loaded_model
 
@@ -689,7 +701,7 @@ def clip_back(
     print("starting boot of clip back")
     if columns_to_return is None:
         columns_to_return = ["url", "image_path", "caption", "NSFW"]
-    safety_model = load_safety_model() if provide_safety_model else None
+    safety_model = load_safety_model(clip_model) if provide_safety_model else None
     print("safety model loaded")
     indices_loaded, indices, device, model, preprocess, mclip_model = load_clip_indices(
         indices_paths,
