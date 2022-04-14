15da4893f4ae89f9c6459ef0e41f3dd68d4a231f
cat-state <cat-state@tutanota.com>
2022-04-14 11:01:19 +0000

    clean up and lint

--- clip_retrieval/clip_inference/load_clip.py
+++ clip_retrieval/clip_inference/load_clip.py
@@ -6,13 +6,13 @@ import clip
 
 
 def load_open_clip(clip_model, use_jit=True, device="cuda"):
-    import open_clip # pylint: disable=import-outside-toplevel
+    import open_clip  # pylint: disable=import-outside-toplevel
+
     pretrained = dict(open_clip.list_pretrained())
     checkpoint = pretrained[clip_model]
-    model, _, preprocess = open_clip.create_model_and_transforms(clip_model,
-                                                                 pretrained=checkpoint,
-                                                                 device=device,
-                                                                 jit=use_jit)
+    model, _, preprocess = open_clip.create_model_and_transforms(
+        clip_model, pretrained=checkpoint, device=device, jit=use_jit
+    )
     return model, preprocess
 
 
@@ -20,7 +20,7 @@ def load_open_clip(clip_model, use_jit=True, device="cuda"):
 def load_clip(clip_model="ViT-B/32", use_jit=True):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if clip_model.startswith("open_clip:"):
-        clip_model = clip_model[len("open_clip:"):]
+        clip_model = clip_model[len("open_clip:") :]
         return load_open_clip(clip_model, use_jit, device)
     else:
         model, preprocess = clip.load(clip_model, device=device, jit=use_jit)
