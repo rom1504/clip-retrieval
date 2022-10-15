7589ff1d191f4085bfdbf7dc30c492fa18fcaddb
nousr <51308183+nousr@users.noreply.github.com>
2022-10-15 22:08:34 +0000

    set torch device

--- clip_retrieval/clip_inference/slurm_worker.py
+++ clip_retrieval/clip_inference/slurm_worker.py
@@ -9,8 +9,9 @@ import os
 import json
 from time import sleep
 
-from clip_retrieval.clip_inference.worker import worker
+from torch.cuda import set_device
 
+from clip_retrieval.clip_inference.worker import worker
 
 def get_task_list(num_tasks, world_size, global_rank):
     """Get the list of tasks to process."""
@@ -31,7 +32,9 @@ def get_task_list(num_tasks, world_size, global_rank):
 
     tasks = list(range(start, end))
 
-    print(f"worker {global_rank} processing tasks {tasks}")
+    local_rank = os.environ["SLURM_LOCALID"]
+    
+    print(f"worker global rank:{global_rank}\tlocal rank: {local_rank}\tprocessing tasks {tasks}")
 
     return tasks
 
@@ -42,7 +45,6 @@ def slurm_worker():
     num_tasks = int(os.environ["NUM_TASKS"])
     global_rank = int(os.environ["SLURM_PROCID"])
     world_size = int(os.environ["WORLD_SIZE"])
-    tasks_per_worker = num_tasks // world_size
 
     # Read the worker args from the file
     with open(os.environ["WORKER_ARGS_PATH"], "r", encoding="utf-8") as worker_args_file:
@@ -51,8 +53,11 @@ def slurm_worker():
     # Find the range of tasks to process
     tasks = get_task_list(num_tasks, world_size, global_rank)
 
-    # Process the tasks
-    worker(tasks=tasks, **worker_args)
+    # set device
+    set_device(int(os.environ["SLURM_LOCALID"]))
+
+    # Launch the worker
+    worker(tasks, **worker_args)
 
 
 if __name__ == "__main__":
