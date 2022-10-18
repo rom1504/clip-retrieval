d0840d2920d4f16b2777deb98556a6b7d5d178d0
nousr <51308183+nousr@users.noreply.github.com>
2022-10-18 23:48:15 +0000

    lint slurm_worker

--- clip_retrieval/clip_inference/slurm_worker.py
+++ clip_retrieval/clip_inference/slurm_worker.py
@@ -7,13 +7,13 @@ Will launch a worker.
 
 import os
 import json
-from time import sleep
 
 from torch.cuda import set_device
 
 from clip_retrieval.clip_inference.worker import worker
 
-def get_task_list(num_tasks, world_size, global_rank):
+
+def get_task_list(num_tasks, world_size, global_rank, local_rank):
     """Get the list of tasks to process."""
     tasks_per_worker = num_tasks // world_size
 
@@ -32,29 +32,30 @@ def get_task_list(num_tasks, world_size, global_rank):
 
     tasks = list(range(start, end))
 
-    local_rank = os.environ["SLURM_LOCALID"]
-    
     print(f"worker global rank:{global_rank}\tlocal rank: {local_rank}\tprocessing tasks {tasks}")
 
     return tasks
 
 
 def slurm_worker():
+    """Distribute work to this job and launch a job."""
+
     # Read environment variables
     # These are set by slurm_distributor or SLURM itself
     num_tasks = int(os.environ["NUM_TASKS"])
     global_rank = int(os.environ["SLURM_PROCID"])
     world_size = int(os.environ["WORLD_SIZE"])
+    local_rank = int(os.environ["SLURM_LOCALID"])
 
     # Read the worker args from the file
     with open(os.environ["WORKER_ARGS_PATH"], "r", encoding="utf-8") as worker_args_file:
         worker_args = json.load(worker_args_file)
 
     # Find the range of tasks to process
-    tasks = get_task_list(num_tasks, world_size, global_rank)
+    tasks = get_task_list(num_tasks, world_size, global_rank, local_rank)
 
     # set device
-    set_device(int(os.environ["SLURM_LOCALID"]))
+    set_device(local_rank)
 
     # Launch the worker
     worker(tasks, **worker_args)
